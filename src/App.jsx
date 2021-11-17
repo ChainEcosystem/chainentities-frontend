52dba60ee68ccfa37c8515903493f869ec9d3dad
yewyewXD <yewyew6933@gmail.com>
2021-11-17 19:24:27 +0100

    add hero section to new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
@@ -7,25 +8,7 @@ const App = () => {
       <Header />
 
       <div className="container mx-auto">
-        {/* hero */}
-        <div className="grid grid-cols-5">
-          <div className="col-span-3">
-            <div>ChainEntities make you styled</div>
-            <div>Data's limited, rush for Your unique token</div>
-
-            <div>
-              The time when everyone and everything lives on blockchain is here.
-            </div>
-
-            <div>
-              <button>Opensea</button>
-
-              <button>Discord</button>
-            </div>
-          </div>
-
-          <div className="col-span-2">something</div>
-        </div>
+        <Hero />
       </div>
     </main>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="grid grid-cols-5">
+      <div className="col-span-3">
+        <div>ChainEntities make you styled</div>
+        <div>Data's limited, rush for Your unique token</div>
+
+        <div>
+          The time when everyone and everything lives on blockchain is here.
+        </div>
+
+        <div>
+          <button>Opensea</button>
+
+          <button>Discord</button>
+        </div>
+      </div>
+
+      <div className="col-span-2">something</div>
+    </section>
+  );
+};
+
+export default Hero;
