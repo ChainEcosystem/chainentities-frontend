b549b16daa3eb642551051b5c3fa15de7a1af7bf
yewyewXD <yewyew6933@gmail.com>
2021-11-18 10:00:00 +0100

    use relevant style class

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,19 @@ const Hero = () => {
   return (
     <section className="grid grid-cols-5">
       <div className="col-span-3">
-        <div>ChainEntities make you styled</div>
-        <div>Data's limited, rush for Your unique token</div>
+        <h2 className="font-bold text-primary">
+          ChainEntities make You styled
+        </h2>
+        <h1>Data's limited, rush for Your unique token</h1>
 
-        <div>
+        <p>
           The time when everyone and everything lives on blockchain is here.
-        </div>
+        </p>
 
-        <div>
-          <button>Opensea</button>
+        <div className="flex">
+          <button className="btn-primary mr-2">Opensea</button>
 
-          <button>Discord</button>
+          <button className="btn-secondary">Discord</button>
         </div>
       </div>
 
