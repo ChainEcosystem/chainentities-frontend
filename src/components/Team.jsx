5c30a4c36db29a0c88451f1cb844a32dbbf2df91
yewyewXD <yewyew6933@gmail.com>
2021-11-18 18:10:57 +0100

    style team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,26 +2,34 @@ import React from "react";
 
 const Team = () => {
   return (
-    <>
-      <div>Team</div>
+    <section className="pt-20">
+      <div className="flex justify-center items-center mb-10">
+        <div className="TitleDivider_Left" />
+        <h1 className="font-bold text-primary mx-7">Team</h1>
+        <div className="TitleDivider_Right" />
+      </div>
 
-      <div className="grid grid-cols-3 grid-rows-1 grid-flow-row">
-        <div>
-          <img src="/images/Team1.png" alt="" />
-          <div>Alex Groapa</div>
-          <div>Artist & Project creator, Senior Product Designer</div>
+      <div className="grid grid-cols-3 grid-rows-1 grid-flow-row items-start">
+        <div className="flex flex-col justify-center items-center px-12">
+          <img src="/images/Team1.png" className="m-auto" alt="" />
+          <h2 className="text-blue my-3 text-center">Alex Groapa</h2>
+          <p className="text-center">
+            Artist & Project creator, Senior Product Designer
+          </p>
         </div>
 
-        <div>
-          <img src="/images/Team2.png" alt="" />
-          <div>Michael Mucerschi</div>
-          <div>Architect, Artist, 3D Modeler, Senior Graphic Designer</div>
+        <div className="flex flex-col justify-center items-center px-12">
+          <img src="/images/Team2.png" className="m-auto" alt="" />
+          <h2 className="text-blue my-3 text-center">Michael Mucerschi</h2>
+          <p className="text-center">
+            Architect, Artist, 3D Modeler, Senior Graphic Designer
+          </p>
         </div>
 
-        <div>
-          <img src="/images/Team3.png" alt="" />
-          <div>Peter Iatco </div>
-          <div>Blockchain Developer </div>
+        <div className="flex flex-col justify-center items-center px-12">
+          <img src="/images/Team3.png" className="m-auto" alt="" />
+          <h2 className="text-blue my-3 text-center">Peter Iatco</h2>
+          <p className="text-center">Blockchain Developer </p>
         </div>
       </div>
 
@@ -51,7 +59,7 @@ const Team = () => {
 
         <div>Thanks for contributing for future of this project!</div>
       </div>
-    </>
+    </section>
   );
 };
 
