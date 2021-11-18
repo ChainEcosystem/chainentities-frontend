687785a05e461a4350635b716c9432a996de0d97
yewyewXD <yewyew6933@gmail.com>
2021-11-18 18:58:13 +0100

    setup collasible state and style

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import Collapsible from "react-collapsible";
 
 const FAQ = () => {
+  const [isOpened1, setIsOpened1] = useState(false),
+    [isOpened2, setIsOpened2] = useState(false),
+    [isOpened3, setIsOpened3] = useState(false);
+
   return (
     <section className="pt-20">
       <div className="flex justify-center items-center mb-10">
@@ -10,19 +15,42 @@ const FAQ = () => {
       </div>
 
       {/* FAQ Box 1 */}
-      <div className="TeamNoteBox cursor-pointer mb-3">
-        <div className="TeamNoteBox__Button">
+
+      <div
+        className="TeamNoteBox cursor-pointer mb-3"
+        onClick={() => setIsOpened1((open) => !open)}
+      >
+        <div
+          className={`TeamNoteBox__Button ${
+            isOpened1 && "TeamNoteBox__Button--active"
+          }`}
+        >
           <img src="/images/VectorCross.svg" alt="" />
         </div>
-        <div className="text-blue font-bold mb-3">
-          <big>What token do You use?</big>
-        </div>
 
-        <p className="text-white">
-          <ul>
-            <li>Our NFT’s are all stored as ERC-721 tokens</li>
-          </ul>
-        </p>
+        <Collapsible
+          transitionTime={200}
+          onOpen={() => {
+            if (!isOpened1) setIsOpened1(true);
+          }}
+          onClose={() => {
+            if (isOpened1) setIsOpened1(false);
+          }}
+          open={isOpened1}
+          trigger={
+            <div className="text-blue font-bold">
+              <big>What token do You use?</big>
+            </div>
+          }
+        >
+          <div className="text-white mt-3 transition duration-200">
+            <ul>
+              <li>
+                <p>Our NFT’s are all stored as ERC-721 tokens</p>
+              </li>
+            </ul>
+          </div>
+        </Collapsible>
       </div>
 
       {/* FAQ Box 2 */}
@@ -30,15 +58,16 @@ const FAQ = () => {
         <div className="TeamNoteBox__Button">
           <img src="/images/VectorCross.svg" alt="" />
         </div>
-        <div className="text-blue font-bold mb-3">
+        <div className="text-blue font-bold">
           <big>Are Your NFT’s hosted?</big>
         </div>
-
-        <p className="text-white">
+        <div className="text-white mt-3 transition duration-200">
           <ul>
-            <li>Yes, all tokens are IPFS hosted, they are here forever</li>
+            <li>
+              <p>Yes, all tokens are IPFS hosted, they are here forever</p>
+            </li>
           </ul>
-        </p>
+        </div>
       </div>
 
       {/* FAQ Box 3 */}
@@ -46,18 +75,20 @@ const FAQ = () => {
         <div className="TeamNoteBox__Button">
           <img src="/images/VectorCross.svg" alt="" />
         </div>
-        <div className="text-blue font-bold mb-3">
+        <div className="text-blue font-bold">
           <big>What’s the blockchain You use?</big>
         </div>
 
-        <p className="text-white">
+        <div className="text-white mt-3 transition duration-200">
           <ul>
             <li>
-              We use Polygon Blockchain. Same security, same scalability, but
-              less gas fees for You
+              <p>
+                We use Polygon Blockchain. Same security, same scalability, but
+                less gas fees for You
+              </p>
             </li>
           </ul>
-        </p>
+        </div>
       </div>
     </section>
   );
