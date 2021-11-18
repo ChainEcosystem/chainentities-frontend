d3eae94d212e4a60b344abd26cdb9bee12c0f2e9
yewyewXD <yewyew6933@gmail.com>
2021-11-18 18:01:22 +0100

    fin style of roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -2,21 +2,123 @@ import React from "react";
 
 const Roadmap = () => {
   return (
-    <section>
-      <div>Roadmap</div>
+    <section className="pt-20">
+      <div className="flex justify-center items-center mb-10">
+        <div className="TitleDivider_Left" />
+        <h1 className="font-bold text-primary mx-7">Roadmap</h1>
+        <div className="TitleDivider_Right" />
+      </div>
 
-      <div className="grid grid-cols-3 grid-rows-2 grid-flow-row gap-6">
-        <div>test</div>
+      <div className="grid grid-cols-3 grid-rows-2 grid-flow-row gap-10">
+        {/* Box 1 */}
+        <div className="RoadmapBox">
+          <div className="RoadmapBox__Title">
+            <big className="mr-2">Q2-4</big>
+            <big>2021</big>
+          </div>
 
-        <div>test</div>
+          <ul>
+            <li>Team Building</li>
+            <li>Project concepting</li>
+            <li>Art creating</li>
+            <li>Blockchain research</li>
+            <li>Smart contract writing</li>
+            <li>Smart contract launch</li>
+          </ul>
+        </div>
 
-        <div>test</div>
+        {/* Box 2 */}
+        <div className="RoadmapBox">
+          <div className="RoadmapBox__Title">
+            <big className="mr-2">Q1</big>
+            <big>2022</big>
+          </div>
 
-        <div>test</div>
+          <ul>
+            <li>Website launch</li>
+            <li>Public minting launch</li>
+            <li>
+              Community 200 NFTs’ giveaway{" "}
+              <span className="text-blue">(January)</span>
+            </li>
+            <li>Social media marketing strategy</li>
+          </ul>
+        </div>
 
-        <div>test</div>
+        {/* Box 3 */}
+        <div className="RoadmapBox">
+          <div className="RoadmapBox__Title">
+            <big className="mr-2">Q2</big>
+            <big>2022</big>
+          </div>
 
-        <div>test</div>
+          <ul>
+            <li>
+              <span className="text-blue">
+                Next art creation for future merging 2+ collections (race, sex,
+                city?)
+              </span>
+            </li>
+            <li>Video presentation for a future ecosystem</li>
+            <li>Roadmap update</li>
+          </ul>
+        </div>
+
+        {/* Box 4 */}
+        <div className="RoadmapBox">
+          <div className="RoadmapBox__Title">
+            <big className="mr-2">Q3</big>
+            <big>2022</big>
+          </div>
+
+          <ul>
+            <li>
+              Giveaway
+              <span className="text-blue"> (Community voting)</span>
+            </li>
+            <li>
+              Whitelist injection and holders discount system for future
+              collections
+            </li>
+            <li>“Outside the wall” concept creating</li>
+            <li>Brand creation</li>
+          </ul>
+        </div>
+
+        {/* Box 5 */}
+        <div className="RoadmapBox">
+          <div className="RoadmapBox__Title">
+            <big className="mr-2">Q4</big>
+            <big>2022</big>
+          </div>
+
+          <ul>
+            <li>Metaverse land buying</li>
+            <li>Building community clusters</li>
+            <li>Gamification project announcement</li>
+            <li>
+              Giveaway
+              <span className="text-blue"> (Community voting)</span>
+            </li>
+          </ul>
+        </div>
+
+        {/* Box 6 */}
+        <div className="RoadmapBox">
+          <div className="RoadmapBox__Title">
+            <big className="mr-2">Q1</big>
+            <big>2023</big>
+          </div>
+
+          <ul>
+            <li>
+              Personalized jackets, caps, “identities” etc. holders giveaway
+            </li>
+            <li>Developing token smart contract</li>
+            <li>Chain Ecosystem platform creating</li>
+            <li>Roadmap update</li>
+          </ul>
+        </div>
       </div>
 
       <div>
