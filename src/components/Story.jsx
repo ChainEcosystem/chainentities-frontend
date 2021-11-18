9ed92eee5fd36fe7cff5df275c414a2c9528be56
yewyewXD <yewyew6933@gmail.com>
2021-11-18 17:21:16 +0100

    style part 2 & 3 of story

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Story = () => {
   return (
-    <section className="pt-24">
+    <section>
       {/* Part 1: New life systems */}
-      <div className="grid grid-cols-5">
+      <div className="grid grid-cols-5 pt-20">
         <div className="col-span-2">
           <img src="/images/HeroImage1.png" alt="" />
         </div>
@@ -30,46 +30,48 @@ const Story = () => {
       </div>
 
       {/* Part 2: ChainEntities is Future */}
-      <div className="grid grid-cols-5">
-        <div className="col-span-3">
-          <div>ChainEntities is Future</div>
+      <div className="grid grid-cols-5 pt-20">
+        <div className="col-span-3 flex flex-col justify-center pr-24">
+          <h2 className="text-primary mb-6">ChainEntities is Future</h2>
 
-          <div>
+          <p>
             Nowadays (~2097) “almost” every human wears a mask, that way their
             identity stays safe. But everyone has a personality to show, that’s
-            why there is ChainEntities.
-          </div>
+            why there is <span className="text-blue">ChainEntities</span>.
+          </p>
 
-          <div>
+          <p className="mt-5">
             After linking with an Entity, You get marked in the city’s
             blockchain with a unique “styled” ERC-721 token. Afterwards,
-            Entities are used for almost everything to prove it’s really YOU.
-          </div>
+            Entities are used for almost everything to prove{" "}
+            <span className="text-blue">it’s really YOU</span>.
+          </p>
 
-          <div>
+          <div className="xsmall text-primary mt-5">
             Be carefull with whom You’re having business, because a single
             creature can mark him/her/it-self with multiple Entities!
           </div>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 flex justify-center items-center">
           <img src="/images/HeroImage2.png" alt="" />
         </div>
       </div>
 
       {/* Part 3: Why Polygon blockchain? */}
-      <div className="grid grid-cols-5">
-        <div className="col-span-2">
+      <div className="grid grid-cols-5 pt-20">
+        <div className="col-span-2 flex items-center justify-center">
           <img src="/images/HeroImage3.png" alt="" />
         </div>
 
-        <div className="col-span-3">
-          <div>Why Polygon blockchain?</div>
+        <div className="col-span-3 flex flex-col justify-center pl-24">
+          <h2 className="text-primary mb-6">Why Polygon blockchain?</h2>
 
-          <div>
-            We stand for equity, so we make it accessible for everyone to get an
-            identity and join a city asap. This way You’ll leave behind “outside
-            the wall” life. Rush and HODL, to be continued...
-          </div>
+          <p>
+            We stand for <span className="text-blue">equity</span>, so we make
+            it accessible for everyone to get an identity and join a city asap.
+            This way You’ll leave behind “outside the wall” life. Rush and HODL,{" "}
+            <span className="text-blue">to be continued...</span>
+          </p>
         </div>
       </div>
     </section>
