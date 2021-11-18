94d9c0f2de2cbead05da6d21a6165f4024ebcd5f
yewyewXD <yewyew6933@gmail.com>
2021-11-18 16:19:20 +0100

    fix margins

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,16 +23,27 @@ const Hero = () => {
       </div>
 
       <div className="col-span-2">
-        <div className="MintContainer h-60 w-80">
+        <div className="MintContainer h-72 w-80">
           <div className="MintContent">
             <big className="block">ChainEntities Minted</big>
             <h2 className="font-bold text-primary">0/4444</h2>
+
+            <hr
+              style={{
+                width: "100%",
+                borderTop: "solid 2px grey",
+                margin: "14px 0",
+              }}
+            />
+
             <big className="block">
               1 Entity costs <span className="text-blue">29</span> Matic
             </big>
-            <small className="text-divider block">Excluding gas fees</small>
+            <small className="text-divider block mt-1">
+              Excluding gas fees
+            </small>
 
-            <div className="flex items-center">
+            <div className="flex items-center mt-6">
               {/* Mint count control */}
               <div
                 className="flex justify-center items-center bg-light w-min"
