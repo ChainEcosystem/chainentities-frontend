6f1b3b9ade99093bce2d0d2d261af06ead95cfee
yewyewXD <yewyew6933@gmail.com>
2021-11-19 10:42:03 +0100

    team responsive

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 const Team = () => {
   return (
-    <section id="team" className="pt-20">
-      <div className="flex justify-center items-center mb-10">
+    <section id="team" className="md:pt-20 pt-14">
+      <div className="flex justify-center items-center md:mb-10 mb-6">
         <div className="TitleDivider_Left" />
         <h1 className="font-bold text-primary mx-7">Team</h1>
         <div className="TitleDivider_Right" />
       </div>
 
       {/* Part 1: Images */}
-      <div className="grid grid-cols-3 grid-rows-1 grid-flow-row items-start">
-        <div className="flex flex-col justify-center items-center px-12">
-          <img src="/images/Team1.png" className="m-auto" alt="" />
-          <h2 className="text-blue my-3 text-center whitespace-nowrap">
+      <div className="md:grid flex flex-col grid-cols-3 grid-rows-1 grid-flow-row md:items-start items-center">
+        <div className="flex flex-col justify-center items-center px-12 md:mb-0 mb-6">
+          <img
+            src="/images/Team1.png"
+            className="m-auto sm:w-auto w-40"
+            alt=""
+          />
+          <h2 className="text-blue md:my-3 my-2 text-center whitespace-nowrap">
             Alex Groapa
           </h2>
           <p className="text-center">
@@ -21,9 +25,13 @@ const Team = () => {
           </p>
         </div>
 
-        <div className="flex flex-col justify-center items-center px-12">
-          <img src="/images/Team2.png" className="m-auto" alt="" />
-          <h2 className="text-blue my-3 text-center whitespace-nowrap">
+        <div className="flex flex-col justify-center items-center px-12 md:mb-0 mb-6">
+          <img
+            src="/images/Team2.png"
+            className="m-auto sm:w-auto w-40"
+            alt=""
+          />
+          <h2 className="text-blue md:my-3 my-2 text-center whitespace-nowrap">
             Michael Mucerschi
           </h2>
           <p className="text-center">
@@ -32,8 +40,12 @@ const Team = () => {
         </div>
 
         <div className="flex flex-col justify-center items-center px-12">
-          <img src="/images/Team3.png" className="m-auto" alt="" />
-          <h2 className="text-blue my-3 text-center whitespace-nowrap">
+          <img
+            src="/images/Team3.png"
+            className="m-auto sm:w-auto w-40"
+            alt=""
+          />
+          <h2 className="text-blue md:my-3 my-2 text-center whitespace-nowrap">
             Peter Iatco
           </h2>
           <p className="text-center">Blockchain Developer </p>
@@ -41,7 +53,7 @@ const Team = () => {
       </div>
 
       {/* Part 2: Note */}
-      <div className="TeamNoteBox mt-10">
+      <div className="TeamNoteBox md:mt-10 mt-6">
         <div className="text-blue">
           <big>
             We’ve got a few more niche experts, but they asked to keep their
@@ -49,8 +61,8 @@ const Team = () => {
           </big>
         </div>
 
-        <div className="grid grid-cols-2 my-5">
-          <ul className="col-span-1 grid grid-cols-2 grid-rows-4 gap-x-6 text-white">
+        <div className="grid sm:grid-cols-2 grid-cols-1 md:my-5 my-3">
+          <ul className="col-span-1 grid sm:grid-cols-2 grid-cols-1 grid-rows-4 gap-x-6 text-white">
             <li>Blockchain developer</li>
             <li>Game Designer</li>
             <li>FullStack Developer x3</li>
