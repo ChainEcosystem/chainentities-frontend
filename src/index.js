cca77c977a54e3511edab92e3e946c0de1abc39e
yewyewXD <yewyew6933@gmail.com>
2021-11-19 09:17:26 +0100

    install & use react-multi-carousel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/final.scss";
+import "react-multi-carousel/lib/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
