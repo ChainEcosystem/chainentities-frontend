6f48a6dca1e3df5213e69ad3e46633ba25f3a608
yewyewXD <yewyew6933@gmail.com>
2021-11-19 12:28:51 +0100

    fix navbar keep showing on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -98,9 +98,9 @@ const Header = () => {
 
       {/* Mobile nav  */}
       <nav
-        className={`md:hidden absolute bg-dark py-3 px-2 transition duration-200 w-full h-full bg-white z-20 transform -translate-y-${
-          isMenuOpen ? "0" : "full"
-        }`}
+        className={`MobileNav ${
+          isMenuOpen && "MobileNav--activated"
+        } md:hidden absolute bg-dark py-3 px-2 transition duration-200 w-full h-full bg-white z-20 transform`}
       >
         <div className="container mx-auto flex flex-col justify-center text-white">
           <div
