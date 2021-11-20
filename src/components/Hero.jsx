216c33fdced1fcaa4ec61dc5a5879d1297a15f84
yewyewXD <yewyew6933@gmail.com>
2021-11-20 09:35:37 +0100

    show sliding notification on web3 connect success

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -60,7 +60,7 @@ const Hero = () => {
 
     let timeoutId = setTimeout(() => {
       setShowWeb3ConnectionSuccess(false);
-    }, 3000);
+    }, 5000);
 
     return () => clearTimeout(timeoutId);
   }, [isWeb3Enabled]);
@@ -98,193 +98,211 @@ const Hero = () => {
   }, [user?.attributes?.ethAddress, isWeb3Enabled]);
 
   return (
-    <section className="HeroSection">
-      <div className="flex flex-col justify-center md:items-start items-center md:my-0 my-5 lg:pr-24 md:pr-12">
-        <h2 className="font-bold text-primary mb-1 md:text-left text-center">
-          ChainEntities make You styled
-        </h2>
-        <h1 className="md:text-left text-center">
-          Data's limited, rush for Your <br className="sm:block hidden" />
-          unique token
-        </h1>
+    <>
+      <div
+        className={`flex justify-center transition duration-300 fixed left-0 z-50 w-full -top-10 transform ${
+          showWeb3ConnectionSuccess ? "translate-y-20" : ""
+        } `}
+      >
+        <div className="flex justify-center items-center py-3 px-4 bg-dark">
+          <img src="/images/VectorCheckmark.svg" className="mr-3" alt="" />
 
-        <p className="mt-3 md:text-left text-center">
-          The time when everyone and everything <br /> lives on blockchain is
-          here.
-        </p>
-
-        {!isOnMobile && (
-          <div className="flex mt-5">
-            <button
-              className="btn-primary NavItemSpacing"
-              onClick={() => {
-                redirectSocialLink("https://discord.gg/5qRS9KThV2");
-              }}
-            >
-              Opensea
-            </button>
-            <button
-              className="btn-secondary"
-              onClick={() => {
-                redirectSocialLink("https://discord.gg/5qRS9KThV2");
-              }}
-            >
-              Discord
-            </button>
-          </div>
-        )}
-      </div>
-
-      <div className="relative">
-        {(mintData || mintError || web3EnableError) && (
-          <div
-            style={{ bottom: "-76px" }}
-            className="absolute rounded-lg w-full bg-divider text-white p-4"
-          >
-            {(mintError || web3EnableError) && (
-              <span className="mt-1 xsmall">
-                Sorry, something went wrong,{" "}
-                <span
-                  className="text-pink transition duration-200 cursor-pointer hover:text-white"
-                  onClick={() => window.location.reload()}
-                >
-                  refresh
-                </span>{" "}
-                the page and try again!
-              </span>
-            )}
-
-            {mintData && (
-              <span className="mt-1 xsmall">
-                Congratulations! You’ve got Your own Entity
-                <br />
-                Visit{" "}
-                <span
-                  className="text-pink cursor-pointer"
-                  onClick={() =>
-                    redirectSocialLink("https://discord.gg/5qRS9KThV2")
-                  }
-                >
-                  opensea
-                </span>{" "}
-                to view it!
-              </span>
-            )}
-          </div>
-        )}
-
-        <div
-          className="MintBoxContainer"
-          style={{ ...(isOnMobile ? { height: "233px" } : {}) }}
-        >
-          <div className="MintBox">
-            <big className="block">ChainEntities Minted</big>
-            {/* Minted count */}
-            <h2 className="font-bold text-primary mt-3">{totalMinted}/4444</h2>
-
-            <hr className="MintBox__Divider" />
-
-            <big className="block">
-              {mintCount} Entity costs{" "}
-              <span className="text-blue">
-                {(+mintCount * +contract.test.cost).toFixed(4)}
-              </span>{" "}
-              Matic
-            </big>
-            <small className="text-divider block md:mt-2 mt-1">
-              Excluding gas fees
-            </small>
-
-            {!isOnMobile && (
-              <div className="flex items-center mt-6">
-                <div className="MintCount">
-                  {/* Minus button */}
-                  <div
-                    disabled={mintIsFetching || mintIsLoading}
-                    className="MintCount__Button"
-                    onClick={() => {
-                      setMintCount((prevCount) => {
-                        if (prevCount <= 1) return prevCount;
-                        return prevCount - 1;
-                      });
-                    }}
-                  >
-                    <div className="MintCount__ButtonInner">
-                      <big className="text-primary">-</big>
-                    </div>
-                  </div>
-
-                  {/* Mint counter */}
-                  <div className="MintCount__Indicator">
-                    <big>{mintCount}</big>
-                  </div>
-
-                  {/* Plus button */}
-                  <div
-                    disabled={mintIsFetching || mintIsLoading}
-                    className="MintCount__Button"
-                    onClick={() => {
-                      setMintCount((prevCount) => {
-                        if (prevCount >= 13) return prevCount;
-                        return prevCount + 1;
-                      });
-                    }}
-                  >
-                    <div className="MintCount__ButtonInner">
-                      <big className="text-primary">+</big>
-                    </div>
-                  </div>
-                </div>
-
-                {/* Mint button */}
-                <button
-                  disabled={mintIsFetching || mintIsLoading}
-                  onClick={async () => {
-                    if (user?.attributes?.ethAddress && isWeb3Enabled) {
-                      console.log("@@@ Minting");
-                      handleMint({
-                        onSuccess: () =>
-                          setTotalMinted((count) => +count + +mintCount),
-                      });
-                    } else {
-                      console.log("@@@ Authenticating");
-                      enableWeb3();
-                    }
-                  }}
-                  className={`MintBox__MintButton ${
-                    mintIsFetching || mintIsLoading
-                      ? "bg-divider text-white rounded-lg MintBox__MintButton--loading"
-                      : "btn-primary"
-                  }`}
-                >
-                  {mintIsFetching || mintIsLoading ? "...Minting" : "Mint"}
-                </button>
-              </div>
-            )}
-
-            {/* Connection warning */}
-            <div className="flex items-start mt-5">
-              <img
-                src="/images/VectorCheckmark.svg"
-                className="md:mt-1 mr-3 w-3"
-                alt=""
-              />
-
-              {isOnMobile ? (
-                <span className="xsmall">
-                  Please visit Web version of the site, to mint an Entity
-                </span>
-              ) : (
-                <span className="xsmall">
-                  Please make sure You are connected to the{" "}
-                  <span className="text-blue">Polygon Network</span>
-                </span>
-              )}
-            </div>
+          <div className="xsmall text-white">
+            Successfully connected to Your wallet
           </div>
         </div>
       </div>
-    </section>
+
+      <section className="HeroSection">
+        <div className="flex flex-col justify-center md:items-start items-center md:my-0 my-5 lg:pr-24 md:pr-12">
+          <h2 className="font-bold text-primary mb-1 md:text-left text-center">
+            ChainEntities make You styled
+          </h2>
+          <h1 className="md:text-left text-center">
+            Data's limited, rush for Your <br className="sm:block hidden" />
+            unique token
+          </h1>
+
+          <p className="mt-3 md:text-left text-center">
+            The time when everyone and everything <br /> lives on blockchain is
+            here.
+          </p>
+
+          {!isOnMobile && (
+            <div className="flex mt-5">
+              <button
+                className="btn-primary NavItemSpacing"
+                onClick={() => {
+                  redirectSocialLink("https://discord.gg/5qRS9KThV2");
+                }}
+              >
+                Opensea
+              </button>
+              <button
+                className="btn-secondary"
+                onClick={() => {
+                  redirectSocialLink("https://discord.gg/5qRS9KThV2");
+                }}
+              >
+                Discord
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className="relative">
+          {(mintData || mintError || web3EnableError) && (
+            <div
+              style={{ bottom: "-76px" }}
+              className="absolute rounded-lg w-full bg-divider text-white p-4"
+            >
+              {(mintError || web3EnableError) && (
+                <span className="mt-1 xsmall">
+                  Sorry, something went wrong,{" "}
+                  <span
+                    className="text-pink transition duration-200 cursor-pointer hover:text-white"
+                    onClick={() => window.location.reload()}
+                  >
+                    refresh
+                  </span>{" "}
+                  the page and try again!
+                </span>
+              )}
+
+              {mintData && (
+                <span className="mt-1 xsmall">
+                  Congratulations! You’ve got Your own Entity
+                  <br />
+                  Visit{" "}
+                  <span
+                    className="text-pink cursor-pointer"
+                    onClick={() =>
+                      redirectSocialLink("https://discord.gg/5qRS9KThV2")
+                    }
+                  >
+                    opensea
+                  </span>{" "}
+                  to view it!
+                </span>
+              )}
+            </div>
+          )}
+
+          <div
+            className="MintBoxContainer"
+            style={{ ...(isOnMobile ? { height: "233px" } : {}) }}
+          >
+            <div className="MintBox">
+              <big className="block">ChainEntities Minted</big>
+              {/* Minted count */}
+              <h2 className="font-bold text-primary mt-3">
+                {totalMinted}/4444
+              </h2>
+
+              <hr className="MintBox__Divider" />
+
+              <big className="block">
+                {mintCount} Entity costs{" "}
+                <span className="text-blue">
+                  {(+mintCount * +contract.test.cost).toFixed(4)}
+                </span>{" "}
+                Matic
+              </big>
+              <small className="text-divider block md:mt-2 mt-1">
+                Excluding gas fees
+              </small>
+
+              {!isOnMobile && (
+                <div className="flex items-center mt-6">
+                  <div className="MintCount">
+                    {/* Minus button */}
+                    <div
+                      disabled={mintIsFetching || mintIsLoading}
+                      className="MintCount__Button"
+                      onClick={() => {
+                        setMintCount((prevCount) => {
+                          if (prevCount <= 1) return prevCount;
+                          return prevCount - 1;
+                        });
+                      }}
+                    >
+                      <div className="MintCount__ButtonInner">
+                        <big className="text-primary">-</big>
+                      </div>
+                    </div>
+
+                    {/* Mint counter */}
+                    <div className="MintCount__Indicator">
+                      <big>{mintCount}</big>
+                    </div>
+
+                    {/* Plus button */}
+                    <div
+                      disabled={mintIsFetching || mintIsLoading}
+                      className="MintCount__Button"
+                      onClick={() => {
+                        setMintCount((prevCount) => {
+                          if (prevCount >= 13) return prevCount;
+                          return prevCount + 1;
+                        });
+                      }}
+                    >
+                      <div className="MintCount__ButtonInner">
+                        <big className="text-primary">+</big>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Mint button */}
+                  <button
+                    disabled={mintIsFetching || mintIsLoading}
+                    onClick={async () => {
+                      if (user?.attributes?.ethAddress && isWeb3Enabled) {
+                        console.log("@@@ Minting");
+                        handleMint({
+                          onSuccess: () =>
+                            setTotalMinted((count) => +count + +mintCount),
+                        });
+                      } else {
+                        console.log("@@@ Authenticating");
+                        enableWeb3();
+                      }
+                    }}
+                    className={`MintBox__MintButton ${
+                      mintIsFetching || mintIsLoading
+                        ? "bg-divider text-white rounded-lg MintBox__MintButton--loading"
+                        : "btn-primary"
+                    }`}
+                  >
+                    {mintIsFetching || mintIsLoading ? "...Minting" : "Mint"}
+                  </button>
+                </div>
+              )}
+
+              {/* Connection warning */}
+              <div className="flex items-start mt-5">
+                <img
+                  src="/images/VectorCheckmark.svg"
+                  className="md:mt-1 mr-3 w-3"
+                  alt=""
+                />
+
+                {isOnMobile ? (
+                  <span className="xsmall">
+                    Please visit Web version of the site, to mint an Entity
+                  </span>
+                ) : (
+                  <span className="xsmall">
+                    Please make sure You are connected to the{" "}
+                    <span className="text-blue">Polygon Network</span>
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
