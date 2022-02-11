3c5ba32e0d5d11df716eef3bc2d9969c24ae3151
yewyewXD <yewyew6933@gmail.com>
2022-02-11 15:58:25 +0800

    setup metadetails section

---
+++ components/03-Details.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Detail = () => {
+  return (
+    <section className="pb-24">
+      <div className="flex justify-center items-center mb-6">
+        <div className="TitleDivider_Left" />
+        <h1 className="font-bold text-primary mx-7">Meta-details</h1>
+        <div className="TitleDivider_Right" />
+      </div>
+    </section>
+  );
+};
+
+export default Detail;
