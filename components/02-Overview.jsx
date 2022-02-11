04739705a78c8140576ee92fa04611fae039d91a
yewyewXD <yewyew6933@gmail.com>
2022-02-11 15:33:42 +0800

    setup overview images & stylesheet

--- components/02-Overview.jsx
+++ components/02-Overview.jsx
@@ -3,11 +3,15 @@ import React from "react";
 const Overview = () => {
   return (
     <section>
-      <div className="flex justify-center items-center md:mb-10 mb-6">
+      <div className="flex justify-center items-center mb-6">
         <div className="TitleDivider_Left" />
         <h1 className="font-bold text-primary mx-7">Overview</h1>
         <div className="TitleDivider_Right" />
       </div>
+
+      <h2 className="text-center mb-14">
+        An ecosystem with a continuous development towards perfection
+      </h2>
     </section>
   );
 };
