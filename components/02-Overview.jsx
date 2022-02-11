66266bbea3627eda863783a03126cc48ef25c823
yewyewXD <yewyew6933@gmail.com>
2022-02-11 15:51:12 +0800

    overview section done

--- components/02-Overview.jsx
+++ components/02-Overview.jsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import React from "react";
+import overviewBlocks from "../json/overviewBlocks.json";
 
 const Overview = () => {
   return (
-    <section>
+    <section className="pb-24">
       <div className="flex justify-center items-center mb-6">
         <div className="TitleDivider_Left" />
         <h1 className="font-bold text-primary mx-7">Overview</h1>
@@ -12,6 +14,46 @@ const Overview = () => {
       <h2 className="text-center mb-14">
         An ecosystem with a continuous development towards perfection
       </h2>
+
+      <div className="flex justify-between">
+        {overviewBlocks.map((block, index) => (
+          <div key={`overview${index}`}>
+            <div className="AboutBox">
+              <div className="flex flex-col justify-center items-start">
+                <Image
+                  className="pointer-events-none"
+                  src={`/images/${block.source}.svg`}
+                  alt=""
+                  height={32}
+                  width={32}
+                />
+                <p className="text-blue mt-2">{block.title}</p>
+                <small className="mt-2">{block.description}</small>
+                <div className="w-full h-20 relative mt-3">
+                  <Image
+                    className="pointer-events-none"
+                    src={`/images/${block.buttonSrc}.svg`}
+                    alt=""
+                    layout="fill"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="AboutBox flex-col mt-8" style={{ width: "100%" }}>
+        <div className="w-full">
+          <p className="text-blue mb-2">More content coming soon...</p>
+        </div>
+        <div className="w-full">
+          <small>
+            Stay tuned, we’re about to launch ChainEntities native token, city
+            governance system and more...
+          </small>
+        </div>
+      </div>
     </section>
   );
 };
