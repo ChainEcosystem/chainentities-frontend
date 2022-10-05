c16242496ba14822aeec58f2c8c81bf32e4afd87
yewyewXD <yewyew6933@gmail.com>
2022-10-05 10:31:16 +0800

    dont init moralis

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,7 @@ import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider
-      appId="UTWiusSu2P8dn1H8LK5Q6N4vC7w1wqjsU0nE6peb"
-      serverUrl="https://aefj6iraiwkm.usemoralis.com:2053/server"
-    >
+    <MoralisProvider initializeOnMount={false}>
       <Component {...pageProps} />
     </MoralisProvider>
   );
