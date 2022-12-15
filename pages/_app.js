20e07dfd85cbb5557b6c91abfcf9d940720d41ef
yewyewXD <yewyew6933@gmail.com>
2022-12-15 08:04:12 +0800

    install, init thirdweb

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/global.scss";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
 export default MyApp;
