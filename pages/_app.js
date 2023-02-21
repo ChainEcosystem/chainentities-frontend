9265b4801ceca8bdb05340a6bda173bd7f49c510
yewyewXD <yewyew6933@gmail.com>
2023-02-21 11:29:17 +0800

    add correct providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,36 @@
 import "../styles/global.scss";
-import { WagmiConfig, createClient } from "wagmi";
-import { getDefaultProvider } from "ethers";
+import "@rainbow-me/rainbowkit/styles.css";
+// import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { mainnet, polygon } from "wagmi/chains";
 
-const client = createClient({
-  autoConnect: true,
-  provider: getDefaultProvider(),
+const { chains, provider } = configureChains(
+  [mainnet, polygon],
+  [
+    alchemyProvider({ apiKey: "tDYIeZGtYN5tXZ81DHc6CxlrUVa9Rdia" }),
+    publicProvider(),
+  ]
+);
+
+// const { connectors } = getDefaultWallets({
+//   appName: "Chain Entities Minting",
+//   chains,
+// });
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  // connectors,
+  provider,
 });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <WagmiConfig client={client}>
+    <WagmiConfig client={wagmiClient}>
+      {/* <RainbowKitProvider chains={chains}> */}
       <Component {...pageProps} />
+      {/* </RainbowKitProvider> */}
     </WagmiConfig>
   );
 }
